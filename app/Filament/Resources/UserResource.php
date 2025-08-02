934707d47ffa6274951758b4f4e6b001256a90e7
Yuri <yuri.young@qq.com>
2025-08-02 18:28:56 +0800

    initial commit

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->email()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->required()
+                    ->minLength(8)
+                    ->maxLength(255)
+                    ->password(),
+
+                Forms\Components\TextInput::make('array')
+                    ->columnSpanFull()
+                    ->hint('TextInput')
+                    ->placeholder('Data in Array format: ["key" => "value", "foo" => "bar"]'),
+
+                Forms\Components\Textarea::make('Json')
+                    ->columnSpanFull()
+                    ->hint('Textarea')
+                    ->helperText('No escaped double quotes but single quotes are fine.')
+                    ->placeholder('Meta data in JSON format: {\'key\': \'value\', \"foo\": \"bar\"}'),
+
+                Forms\Components\RichEditor::make('description')
+                    ->columnSpanFull()
+                    ->hint('RichEditor')
+                    ->helperText('Double quotes are fine.')
+                    ->placeholder('Meta data in JSON format: {"key": "value", "foo": \"bar\"}'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
